074ed81b2b532c20d1f469999a5f57f34b8a2335
Kevin Pagliuca <kevinpagliuca@live.com>
2020-06-09 23:23:49 -0300

    Funcionalidades avançadas

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,9 +17,16 @@ class PointsController {
     .where('city', String(city))
     .where('uf', String(uf))
     .distinct()
-    .select('points.*')
+    .select('points.*');
 
-    return res.json(points);  
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.1.102:3333/uploads/${point.image}`,
+      };  
+    });
+
+    return res.json(serializedPoints);  
   }
 
   async show(req: Request, res: Response){
@@ -31,12 +38,17 @@ class PointsController {
       return res.status(400).json({ message: 'Point not found.'});
     }
 
+    const serializedPoint = {      
+        ...point,
+        image_url: `http://192.168.1.102:3333/uploads/${point.image}`,
+      };     
+
     const items = await knex('items')
     .join('point_items', 'items.id', '=', 'point_items.item_id')
     .where('point_items.point_id', id)
     .select('items.title');
 
-    return res.json({point, items});    
+    return res.json({point: serializedPoint, items});    
   }
 
   async create(req: Request, res: Response) {
@@ -54,7 +66,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
@@ -69,7 +81,10 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: Number) => {
+    const pointItems = items
+    .split(',')
+    .map((item: string) => Number(item.trim()))    
+    .map((item_id: Number) => {
       return {
         item_id,
         point_id: point_id,
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
@@ -45,8 +47,10 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [SelectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
@@ -131,15 +135,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    
+    if(SelectedFile){
+      data.append('image', SelectedFile);
     }
 
     await api.post('points', data);
@@ -158,6 +166,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
